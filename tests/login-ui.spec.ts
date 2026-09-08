f4e52570de8562b37772f599e1c1378d99ea74f5
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-08 13:27:53 +0530

    Updated test method for placeholder

--- tests/login-ui.spec.ts
+++ tests/login-ui.spec.ts
@@ -4,16 +4,20 @@ import { test, expect } from "@playwright/test"
 test.describe("login ui component check", async () => {
 
     test("verify title", async ({ page }) => {
-        await page.goto("https://demo.openemr.io/b/openemr/");
+        await page.goto("");
         await expect(page).toHaveTitle("OpenEMR Login");
     })
 
     test("verify header", async ({ page }) => {
-        await page.goto("https://demo.openemr.io/b/openemr/");
+        await page.goto("");
         await expect(page.locator("xpath=//p[contains(text(),'most popular')]"))
-        .toHaveText("The most popular open-source Electronic Health Record and Medical Practice Management solution.");
+            .toHaveText("The most popular open-source Electronic Health Record and Medical Practice Management solution.");
     })
 
-
+    test("verify placeholder for username and password", async ({ page }) => {
+        await page.goto("");
+        await expect(page.locator("xpath=//input[@id='authUser']")).toHaveAttribute("placeholder", "Username")
+        await expect(page.locator("xpath=//input[@id='clearPass']")).toHaveAttribute("placeholder", "Password")
+    })
 
 })
