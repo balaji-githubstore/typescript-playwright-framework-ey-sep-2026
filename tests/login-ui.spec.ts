f2558815483b4d1995fc0f2bb47cd77f18d3a172
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-08 12:45:05 +0530

    Added Title Check Method

--- tests/login-ui.spec.ts
+++ tests/login-ui.spec.ts
@@ -1,11 +1,18 @@
 import { test, expect } from "@playwright/test"
 
 
-test.describe("login ui component check", () => {
+test.describe("login ui component check", async () => {
 
     test("verify title", async ({ page }) => {
         await page.goto("https://demo.openemr.io/b/openemr/");
         await expect(page).toHaveTitle("OpenEMR Login");
     })
+
+    test("verify header", async ({ page }) => {
+
+        //assert the header - The most popular open-source Electronic Health Record and Medical Practice Management solution.
+    })
+
+
 
 })
