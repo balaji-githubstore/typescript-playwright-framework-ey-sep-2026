b3c9efec0df0bb14d27c2d0cc53e470a9a106e3d
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-08 12:24:07 +0530

    Updated assertion for the creaed test method

--- tests/login-ui.spec.ts
+++ tests/login-ui.spec.ts
@@ -1,12 +1,11 @@
-import { test,expect } from "@playwright/test"
-import assert from 'node:assert';
+import { test, expect } from "@playwright/test"
 
-test.describe("login ui component check",()=>{
 
-    test("verify title", async ({ page }) =>{
+test.describe("login ui component check", () => {
+
+    test("verify title", async ({ page }) => {
         await page.goto("https://demo.openemr.io/b/openemr/");
-        let actualTitle:string=await page.title();
-        assert.deepStrictEqual(actualTitle,"OpenEMR");
+        await expect(page).toHaveTitle("OpenEMR Login");
     })
 
 })
