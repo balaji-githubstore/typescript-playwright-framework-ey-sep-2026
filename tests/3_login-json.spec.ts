23e25693d0d6482fda1c0d3cab76f4c28453c6a7
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-10 13:37:31 +0530

    Update the valid login test using json

--- tests/3_login-json.spec.ts
+++ tests/3_login-json.spec.ts
@@ -4,10 +4,22 @@ import { JsonUtils } from "../utils/json-utils.js"
 
 test.describe("login functionality check", async () => {
 
+    //try using data driven from json key - validLoginData
+    for (const { username, password, language, expectedValue } of validLoginData) {
 
-    const jsonInvalidData=JsonUtils.getJsonValue("invalidLoginData")
+        test(`verify valid login ${username} and ${password}`, async ({ page }) => {
+            await page.goto("");
+            await page.locator("xpath=//input[@id='authUser']").fill(username);
+            await page.locator("xpath=//input[@id='clearPass']").fill(password);
+            await page.locator("xpath=//select[@name='languageChoice']").selectOption({ label: language });
+            await page.locator("xpath=//button[@id='login-button']").click();
+            // Assert the Calendar text 
+            await expect(page.locator("xpath=//span[text()='Calendar']")).toHaveText(expectedValue);
+        })
+    }
 
-
+    //data driven using json - key - invalidLoginData
+    const jsonInvalidData = JsonUtils.getJsonValue("invalidLoginData")
     for (const { username, password, language, expectedValue } of jsonInvalidData) {
 
         test(`verify invalid login1 ${username} and ${password}`, async ({ page }) => {
