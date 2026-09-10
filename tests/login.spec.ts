9eba0e3d93988f23285ed9fe78feee9603e831dd
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-10 12:51:06 +0530

    Data Driven updated for invalid login test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test"
-import { validLoginData } from "../utils/data-source.js"
+import { validLoginData, invalidLoginData } from "../utils/data-source.js"
 
 test.describe("login functionality check", async () => {
 
@@ -17,20 +17,19 @@ test.describe("login functionality check", async () => {
     }
 
 
-    /*
-    john,john123,English (Indian),Invalid username or password
-    saul,saul123,English (Indian),Invalid username or password
-    */
+    for (const { username, password, language, expectedValue } of invalidLoginData) {
 
-    test("verify invalid login", async ({ page }) => {
-        await page.goto("");
-        await page.locator("xpath=//input[@id='authUser']").fill("john");
-        await page.locator("xpath=//input[@id='clearPass']").fill("john123");
-        await page.locator("xpath=//select[@name='languageChoice']").selectOption({ label: "English (Indian)" });
-        await page.locator("xpath=//button[@id='login-button']").click();
-        // Assert the text - Invalid username or password
-        await expect(page.locator("xpath=//p[contains(text(),'Invalid')]")).toHaveText("Invalid username or password");
-    })
+        test(`verify invalid login ${username} and ${password}`, async ({ page }) => {
+            await page.goto("");
+            await page.locator("xpath=//input[@id='authUser']").fill(username);
+            await page.locator("xpath=//input[@id='clearPass']").fill(password);
+            await page.locator("xpath=//select[@name='languageChoice']").selectOption({ label: language });
+            await page.locator("xpath=//button[@id='login-button']").click();
+            // Assert the text - Invalid username or password
+            await expect(page.locator("xpath=//p[contains(text(),'Invalid')]")).toHaveText(expectedValue);
+        })
+
+    }
 
 
 })
--- utils/data-source.ts
+++ utils/data-source.ts
@@ -3,3 +3,9 @@ export const validLoginData = [
     { username: "admin", password: "pass", language: "English (Indian)", expectedValue: "Calendar" },
     { username: "clinician", password: "clinician", language: "English (Indian)", expectedValue: "Calendar" }
 ]
+
+
+export const invalidLoginData = [
+    { username: "john", password: "john123", language: "English (Indian)", expectedValue: "Invalid username or password" },
+    { username: "saul", password: "saul123", language: "English (Indian)", expectedValue: "Invalid username or password" }
+]
