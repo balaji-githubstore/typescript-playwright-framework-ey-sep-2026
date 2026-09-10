45a2f0e8a7744bfc9925036c375b0dca0d1faa3c
balaji-githubstore <balajidinakaran1@gmail.com>
2026-09-10 13:38:04 +0530

    update the json data driven for valid login

--- tests/3_login-json.spec.ts
+++ tests/3_login-json.spec.ts
@@ -5,18 +5,18 @@ import { JsonUtils } from "../utils/json-utils.js"
 test.describe("login functionality check", async () => {
 
     //try using data driven from json key - validLoginData
-    for (const { username, password, language, expectedValue } of validLoginData) {
+    // for (const { username, password, language, expectedValue } of validLoginData) {
 
-        test(`verify valid login ${username} and ${password}`, async ({ page }) => {
-            await page.goto("");
-            await page.locator("xpath=//input[@id='authUser']").fill(username);
-            await page.locator("xpath=//input[@id='clearPass']").fill(password);
-            await page.locator("xpath=//select[@name='languageChoice']").selectOption({ label: language });
-            await page.locator("xpath=//button[@id='login-button']").click();
-            // Assert the Calendar text 
-            await expect(page.locator("xpath=//span[text()='Calendar']")).toHaveText(expectedValue);
-        })
-    }
+    //     test(`verify valid login ${username} and ${password}`, async ({ page }) => {
+    //         await page.goto("");
+    //         await page.locator("xpath=//input[@id='authUser']").fill(username);
+    //         await page.locator("xpath=//input[@id='clearPass']").fill(password);
+    //         await page.locator("xpath=//select[@name='languageChoice']").selectOption({ label: language });
+    //         await page.locator("xpath=//button[@id='login-button']").click();
+    //         // Assert the Calendar text 
+    //         await expect(page.locator("xpath=//span[text()='Calendar']")).toHaveText(expectedValue);
+    //     })
+    // }
 
     //data driven using json - key - invalidLoginData
     const jsonInvalidData = JsonUtils.getJsonValue("invalidLoginData")
